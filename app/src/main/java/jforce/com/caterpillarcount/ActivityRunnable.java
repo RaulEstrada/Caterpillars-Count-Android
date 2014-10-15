4e6f90f7309150107d1d4fe9125f067ba1fdbb77
Justin Forsyth <justinforsyth@dhcp-v80-101921.wireless.unc.edu>
2014-10-15 10:04:20 -0400

    camera on order page implemented

---
+++ app/src/main/java/jforce/com/caterpillarcount/ActivityRunnable.java
@@ -0,0 +1,100 @@
+package jforce.com.caterpillarcount;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.view.Gravity;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.doomonafireball.betterpickers.widget.AccessibleTextView;
+import com.google.gson.JsonObject;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+/**
+ * Created by justinforsyth on 10/2/14.
+ */
+public class ActivityRunnable implements Runnable {
+
+    private LoginActivity activity;
+    private ProgressDialog progressDialog;
+
+    public ActivityRunnable(LoginActivity activity, ProgressDialog progressDialog){
+        super();
+        this.activity = activity;
+        this.progressDialog = progressDialog;
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            // Here you should write your time consuming task...
+
+            EditText emailField = (EditText) activity.findViewById(R.id.login_emailfield);
+            String emailAddress = emailField.getText().toString();
+            EditText passwordField = (EditText) activity.findViewById(R.id.login_passwordfield);
+            String password = passwordField.getText().toString();
+
+            //ensure valid email format
+            boolean validEmail = emailAddress.matches(LoginActivity.EMAIL_REGEX);
+
+            if(!validEmail){
+                activity.getProgressDialog().dismiss();
+                YoYo.with(Techniques.Shake).playOn(activity.findViewById(R.id.login_emailfield));
+                Toast toast = Toast.makeText(activity.getApplicationContext(), "Invalid email format", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                return;
+            }
+
+
+            //password is entered
+            if (password != null && !password.isEmpty()) {
+
+                JsonObject json = new JsonObject();
+                json.addProperty("email", emailAddress);
+                json.addProperty("password", password);
+
+                //POST to backend
+                Ion.with(activity)
+                        .load(RegistrationActivity.BASE_URL + "users.php")
+                        .setJsonObjectBody(json)
+                        .asJsonObject()
+                        .withResponse()
+                        .setCallback(new LoginCredentialsFutureCallback<Response<JsonObject>>(activity));
+
+            }
+            else{//password is not entered
+                activity.getProgressDialog().dismiss();
+                YoYo.with(Techniques.Shake).playOn(activity.findViewById(R.id.login_passwordfield));
+                Toast toast = Toast.makeText(activity.getApplicationContext(), "Please enter a password", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                return;
+            }
+
+
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            boolean loggedIn = sharedPreferences.getBoolean("loggedIn", false);
+
+            if (loggedIn){
+
+                activity.downloadUser();
+            }
+
+        } catch (Exception e) {
+
+
+
+        }
+        progressDialog.dismiss();
+    }
+}
--- app/src/main/java/jforce/com/caterpillarcount/OrderActivity.java
+++ app/src/main/java/jforce/com/caterpillarcount/OrderActivity.java
@@ -2,19 +2,34 @@ package jforce.com.caterpillarcount;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 
 public class OrderActivity extends Activity {
 
     private String order;
+    private Bitmap pic = null;
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public static final String NUMBERS_ONLY = "^[0-9]*$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +73,17 @@ public class OrderActivity extends Activity {
         spinner0.setOnItemSelectedListener(new OrderSpinnerController());
     }
 
-    public int getLength() {
+    public Integer getLength() {
 
         EditText et = (EditText) findViewById(R.id.length_edittext);
-        int length = Integer.parseInt(et.getText().toString());
+
+        String string = et.getText().toString();
+
+        if(!string.matches(NUMBERS_ONLY)){
+            return null;
+        }
+
+        Integer length = Integer.parseInt(et.getText().toString());
 
         return length;
 
@@ -70,10 +92,17 @@ public class OrderActivity extends Activity {
     }
 
 
-    public int getCount() {
+    public Integer getCount() {
 
         EditText et = (EditText) findViewById(R.id.count_edittext);
-        int count = Integer.parseInt(et.getText().toString());
+
+        String string = et.getText().toString();
+
+        if(!string.matches(NUMBERS_ONLY)){
+            return null;
+        }
+
+        Integer count = Integer.parseInt(et.getText().toString());
         return count;
 
 
@@ -90,12 +119,55 @@ public class OrderActivity extends Activity {
 
     public void submit(View view){
 
+
+
+        Integer length = getLength();
+        Integer count = getCount();
+
+
+
+        if(length == null){
+
+            EditText et = (EditText) findViewById(R.id.count_edittext);
+
+            YoYo.with(Techniques.Shake)
+                    .playOn(et);
+
+
+            Toast toast = Toast.makeText(this, getResources().getString(R.string.order_invalidCount), Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+            return;
+
+        }
+
+
+
+        if(count == null){
+
+            EditText et = (EditText) findViewById(R.id.count_edittext);
+
+            YoYo.with(Techniques.Shake)
+                    .playOn(et);
+
+
+            Toast toast = Toast.makeText(this, getResources().getString(R.string.order_invalidCount), Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+
+            return;
+
+        }
+
         Intent data = new Intent();
 
         data.putExtra("order", getOrder());
         data.putExtra("count", getCount());
         data.putExtra("length", getLength());
         data.putExtra("notes", getNotes());
+        data.putExtra("pic", getPic());
+
         setResult(RESULT_OK, data);
         finish();
 
@@ -108,4 +180,39 @@ public class OrderActivity extends Activity {
     public void setOrder(String order){
         this.order = order;
     }
+
+    public void takePicture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            setPic(imageBitmap);
+            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), imageBitmap);
+
+            Button button = (Button) findViewById(R.id.capture_order_photo_button);
+            button.setText("");
+
+            button.setBackgroundDrawable(drawable);
+        }
+    }
+
+    public Bitmap getPic() {
+        return pic;
+    }
+
+    public void setPic(Bitmap pic) {
+        this.pic = pic;
+    }
+
+
+
 }
